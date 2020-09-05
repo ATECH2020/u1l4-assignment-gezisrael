afc5be4fa957479c1872a0f2946642a7ec9d6804
Israel Zewdie <israel.225610@nv.ccsd.net>
2020-09-05 02:10:31 +0000

    Finished StudentDesks

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 class StudentDesks {
     public static void main(String[] args) {
@@ -7,12 +8,25 @@ class StudentDesks {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-        
+        double classADesks = scanner.nextDouble();
+        double classBDesks = scanner.nextDouble();
+        double classCDesks = scanner.nextDouble();
+
+        double aDesks, bDesks, cDesks;
+        double deskPair = 2;
+        double totalDesks;
+        int convertInt;
 
         /*
          *  your code goes here
          */
+         aDesks = (classADesks / deskPair);
+         bDesks = (Math.round(classBDesks / deskPair));
+         cDesks = (classCDesks / deskPair);
 
+         totalDesks = (aDesks + bDesks + cDesks);
+         
+        System.out.println((int)totalDesks);
 
         // closing the scanner object
         scanner.close();
